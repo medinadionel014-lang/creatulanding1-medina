16079830f4ec7bd42ae49c17edea11106335b01b
Dionel Medina <medinadionel014@gmail.com>
2026-05-21 02:14:34 -0300

    widget de carrito optimizado y listo para entrega

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,28 +1,18 @@
 import { useContext } from "react";
-
 import { Link } from "react-router-dom";
-
 import { CartContext } from "../context/CartContext";
 
 function CartWidget() {
-
-  const { totalQuantity } =
-    useContext(CartContext);
+  const { totalQuantity } = useContext(CartContext);
+  const total = totalQuantity();
 
   return (
-
-    <Link to="/cart">
-
+    <Link to="/cart" style={{ textDecoration: 'none', color: 'inherit' }}>
       <span>
-
-        🛒 {totalQuantity()}
-
+        🛒 {total > 0 && <strong>({total})</strong>}
       </span>
-
     </Link>
-
   );
-
 }
 
 export default CartWidget;
